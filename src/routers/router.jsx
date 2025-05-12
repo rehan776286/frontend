6d868b7b13e212b194976b361cd4f42fdd2053e4
Rehan Khan <rehankhan2mrk@gmail.com>
2025-05-12 17:36:04 +0530

    feature/product

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -3,13 +3,11 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Protecter from "../pages/AuthLayout";
 import ClientLayout from "../ClientPaget/ClientLayout";
-import UploadPage from "../pages/uploadpage";
+
 // Lazy-loaded pages
-const Home = lazy(() => import("../pages/home"));
+
 const Register = lazy(() => import("../pages/registerpage"));
 const OtpVerify = lazy(() => import("../pages/otpVerify"));
-const Dashboard = lazy(() => import("../pages/dashboad"));
-const OrderCart = lazy(() => import("../pages/orderDashboard"));
 const ClientHome = lazy(() => import("../ClientPaget/ClientHome"));
 const ProductPage = lazy(() => import("../ClientPaget/productPage"));
 const OrderPage = lazy(() => import("../ClientPaget/orderpage"));
