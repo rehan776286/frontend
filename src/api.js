1bdc23fbf83548e5ba14cabc69353656ee9c5c9c
Rehan Khan <rehankhan2mrk@gmail.com>
2025-05-15 15:47:06 +0530

    vite problem

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "https://backend-ymyr.onrender.com",
-  // baseURL: "http://localhost:4000",
+  //  baseURL: "https://backend-ymyr.onrender.com",
+  baseURL: "http://localhost:4000",
 
   withCredentials: true,
 });
